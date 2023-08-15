165ea9325caa1cfb876b0d1b180a6be877f95d48
Mlowegene <genemlowe@gmail.com>
2023-08-15 16:46:02 +0300

    remove border on navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import MoonIcon from "../assets/icon-moon.svg";
 export const NavBar = () => {
   return (
     <>
-      <div className="navbar flex justify-between border-2 border-red-800">
+      <div className="navbar flex justify-between">
         <h4 className="header-color text-2xl font-bold">devfinder</h4>
         <p className="flex items-center nav-dark-color">
           DARK
